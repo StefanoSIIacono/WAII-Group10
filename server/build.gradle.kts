33aef02ba982de5b373ee3c5fbf64c0f889effa5
federicopasqua <federico.pasqualini99@gmail.com>
2023-04-18 19:00:27 +0200

    test: added profile service unit test

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.9.3")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ server/src/test/kotlin/com/lab2/server/profiles/ProfileServiceTest.kt
@@ -0,0 +1,144 @@
+package com.lab2.server.profiles
+
+import com.lab2.server.exceptionsHandler.exceptions.DuplicateProfileException
+import com.lab2.server.exceptionsHandler.exceptions.ProfileNotFoundException
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
+
+class ProfileServiceTest {
+    private val repository = mockk<ProfileRepository>()
+
+    @Test
+    fun getAllTest() {
+        // given
+        val profileList = mutableListOf(
+            Profile("mariorossi@gmail.com", "Mario", "Rossi"),
+            Profile("luigiverdi@gmail.com", "Luigi", "Verdi")
+        )
+        every { repository.findAll() } returns profileList
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        val result = service.getAll()
+
+        // then
+        verify(exactly = 1) { repository.findAll() }
+        assertEquals(profileList.map { it.toDTO() }, result)
+    }
+
+    @Test
+    fun getProfileByEmailTest() {
+        // given
+        val profile = Profile("mariorossi@gmail.com", "mario", "ross")
+        every { repository.findByIdOrNull("mariorossi@gmail.com") } returns profile
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        val result = service.getProfileByEmail("mariorossi@gmail.com")
+
+        // then
+        verify(exactly = 1) { repository.findByIdOrNull("mariorossi@gmail.com") }
+        assertEquals(profile.toDTO(), result)
+    }
+
+    @Test
+    fun getNotExistentProfileByEmailTest() {
+        // given
+        every { repository.findByIdOrNull("mariorossi@gmail.com") } returns null
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        val result = service.getProfileByEmail("mariorossi@gmail.com")
+
+        // then
+        verify(exactly = 1) { repository.findByIdOrNull("mariorossi@gmail.com") }
+        assertEquals(null, result)
+    }
+
+    @Test
+    fun insertProfileTest() {
+        // given
+        val profile = Profile("mariorossi@gmail.com", "mario", "ross")
+
+        every { repository.existsById("mariorossi@gmail.com") } returns false
+        every { repository.save(any())  } returns profile
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        service.insertProfile(profile.toDTO())
+
+        // then
+        verify(exactly = 1) { repository.existsById("mariorossi@gmail.com")  }
+        verify(exactly = 1) { repository.save(any())  }
+    }
+
+    @Test
+    fun insertExistingProfileTest() {
+        // given
+        val profile = Profile("mariorossi@gmail.com", "mario", "ross")
+
+        every { repository.existsById("mariorossi@gmail.com") } returns true
+        every { repository.save(any())  } returns profile
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        var exceptionThrown = false
+        try {
+            service.insertProfile(profile.toDTO())
+        } catch (e: DuplicateProfileException) {
+            exceptionThrown = true
+        }
+
+        // then
+
+        verify(exactly = 1) { repository.existsById("mariorossi@gmail.com")  }
+        verify(exactly = 0) { repository.save(any())  }
+        assertEquals(exceptionThrown, true)
+    }
+
+    @Test
+    fun editProfileTest() {
+        // given
+        val profile = Profile("mariorossi@gmail.com", "mario", "rossi")
+
+        every { repository.existsById("mariorossi@gmail.com") } returns true
+        every { repository.save(any())  } returns profile
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        service.editProfile("mariorossi@gmail.com", profile.toDTO())
+
+        // then
+        verify(exactly = 1) { repository.existsById("mariorossi@gmail.com")  }
+        verify(exactly = 1) { repository.save(any())  }
+    }
+
+    @Test
+    fun editNotExistingProfileTest() {
+        // given
+        val profile = Profile("mariorossi@gmail.com", "mario", "rossi")
+
+        every { repository.existsById("mariorossi@gmail.com") } returns false
+        every { repository.save(any())  } returns profile
+
+        val service = ProfileServiceImpl(repository)
+        // when
+        var exceptionThrown = false
+        try {
+            service.editProfile("mariorossi@gmail.com", profile.toDTO())
+        } catch (e: ProfileNotFoundException) {
+            exceptionThrown = true
+        }
+
+        // then
+
+        verify(exactly = 1) { repository.existsById("mariorossi@gmail.com")  }
+        verify(exactly = 0) { repository.save(any())  }
+        assertEquals(exceptionThrown, true)
+    }
+
+}
