c9f9c69dd882efa59fc3c950b23b7980645439f1
StefanoSIIacono <s314601@studenti.polito.it>
2023-05-04 14:54:57 +0200

    Feat: TicketService int and class

---
+++ server/src/main/kotlin/com/lab2/server/ticketing/TicketServiceImpl.kt
@@ -0,0 +1,10 @@
+package com.lab2.server.ticketing
+
+import com.lab2.server.profiles.ProfileRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TicketServiceImpl (private val ticketingRepository: TicketingRepository): TicketService  {
+
+
+}
